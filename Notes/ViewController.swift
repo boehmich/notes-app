a6aa6889167fb67d9f10f817cf99c5f6556be8c6
Michael Böhm <michael_boehm@gmx.at>
2022-01-04 18:25:09 +0100

    Initial Commit

---
+++ Notes/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Notes
+//
+//  Created by Boehmich on 04.01.22.
+//  Copyright © 2022 boehmich. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
